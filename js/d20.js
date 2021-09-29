b51b90325940d88b6deae269f7d2f7441e037570
wenlongzhu <1573511441@qq.com>
2021-09-29 22:03:57 +0800

    d20

---
+++ js/d20.js
@@ -0,0 +1,19 @@
+/**
+ * 通关 https://leetcode-cn.com/problems/top-k-frequent-elements/
+ * @param {number[]} nums
+ * @param {number} k
+ * @return {number[]}
+ */
+var topKFrequent = function (nums, k) {
+  let map = new Map()
+  let arr = [...new Set(nums)]
+
+  nums.map((num) => {
+    if (map.has(num))
+      map.set(num, map.get(num) + 1)
+    else
+      map.set(num, 1)
+  })
+
+  return arr.sort((a, b) => map.get(b) - map.get(a)).slice(0, k)
+};
